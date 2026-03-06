c175a081875c228cd255746693c785b0a1e4b62e
Kyuth <chhornrayuth123@gmail.com>
2026-03-06 22:52:03 +0700

    backup

--- Trip-com.py
+++ Trip-com.py
@@ -10,9 +10,17 @@ import time
 #service = Service(executable_path="chromedriver.exe")
 #driver = webdriver.Chrome(service=service)
 
+#user info ------------------------------------
+Departure = input("Where are you departing from?(city): ")
+Destination = input("Where are you going?(city): ")
+Target_month = input("Which months are you planning to leave?: ")
+Departure_date = input("When are you going?(date):") #if sth goes wrong delete these
+Arrival_date = input("When are you returning?(date):")
+
 #used chrome-manager cuz i keep losing chromedriver.exe
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
+
 
 wait = WebDriverWait(driver, 10)
 driver.get("https://www.trip.com/")
@@ -39,7 +47,7 @@ wait.until(
 
 leaving = driver.find_element(By.XPATH, "//input[contains(@placeholder,'Leaving')]")
 leaving.click()
-leaving.send_keys("Tokyo")
+leaving.send_keys(Departure.capitalize())
 
 #clicking the first option after input
 wait.until(
@@ -55,7 +63,7 @@ wait.until(
 
 leaving = driver.find_element(By.XPATH, "//input[contains(@placeholder,'Going')]")
 leaving.click()
-leaving.send_keys("seoul")
+leaving.send_keys(Destination.capitalize())
 
 #clicking the first option after input
 wait.until(
@@ -64,50 +72,50 @@ wait.until(
 result = driver.find_element(By.CLASS_NAME, "poi-result__title")
 result.click()
 
-current_month = "january"
-current_date = "12"
-
-target_month = "march"
-
-#value system for months to be compared
-months = [
-    "January",    # 1
-    "February",   # 2
-    "March",      # 3
-    "April",      # 4
-    "May",        # 5
-    "June",       # 6
-    "July",       # 7
-    "August",     # 8
-    "September",  # 9
-    "October",    # 10
-    "November",   # 11
-    "December"    # 12
-]
-
-current_month_value = months.index(current_month.capitalize())
-target_month_value = months.index(target_month.capitalize())
+#clicking the calendar
 wait.until(
-    EC.element_to_be_clickable((By.CLASS_NAME, "verticalCenter-wrapper"))
+    EC.presence_of_element_located((By.XPATH, "//input[contains(@placeholder,'Any date')]"))
 )
-calendar = driver.find_element(By.CLASS_NAME, "verticalCenter-wrapper")
+calendar = driver.find_element(By.XPATH, "//input[contains(@placeholder,'Any date')]")
 calendar.click()
-wait.until(
-    EC.presence_of_element_located((By.CLASS_NAME, "c-calendar-month__title"))
-)
-date = driver.find_element(By.CLASS_NAME, "c-calendar-month__title").text
-print(date)
 
+#logic for finding target month -----------------------------
+while True:
 
+    wait.until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "c-fuzzy-calendar-month__title"))
+    )
+    month = driver.find_elements(By.CLASS_NAME, "c-fuzzy-calendar-month__title")
 
+    current_month = month[0].text
+    current_month_used = current_month.split()[0]
 
+    months = [
+        "January",    # 1
+        "February",   # 2
+        "March",      # 3
+        "April",      # 4
+        "May",        # 5
+        "June",       # 6
+        "July",       # 7
+        "August",     # 8
+        "September",  # 9
+        "October",    # 10
+        "November",   # 11
+        "December"    # 12
+    ]
 
+    current_month_used_value = months.index(current_month_used.capitalize())
+    Target_month_value = months.index(Target_month.capitalize())
 
-
-
-
-
-
+    if current_month_used_value == Target_month_value:
+        break
+    elif current_month_used_value + 1 <= Target_month_value:
+        wait.until(
+            EC.presence_of_element_located((By.CLASS_NAME, "c-fuzzy-calendar-icon-next"))
+        )
+        next_icon = driver.find_element(By.CLASS_NAME, "c-fuzzy-calendar-icon-next")
+        next_icon.click()
 
 
 
